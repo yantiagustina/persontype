c4d863c6a3afc37bc27e5d9acbf3fea058027dd1
unknown <yantiagustina>
2013-09-19 22:34:24 -0700

    first commit

---
+++ person.java
@@ -0,0 +1,11 @@
+class person
+{
+		public static void main(String[] abc)
+		{
+			int x = 10;
+			char y;
+			y='A';
+			System.out.println("Menampilkan Variable x "+x);
+			System.out.println("Menampilkan Variable y "+y);
+		}
+}
